f127571b8aaa4434e8ca6b8aed66bcc534433f7d
Rossey Charleston <rossey.charleston@handshake.com>
2017-02-16 10:33:11 -0500

    Made tweaks based on Shon's sugesstions

--- boompy/actions.py
+++ boompy/actions.py
@@ -6,13 +6,12 @@ import boompy
 from .base_api import API
 from .errors import UnauthorizedError, BoomiError
 
-PROVISION_FIELDS = ["name", "street", "city", "stateCode", "zipCode",
-                      "countryCode", "status", "product"]
+api = API()
 
 def getAssignableRoles():
     """ Returns a list of assignable Role objects. """
     results = []
-    res = API().https_request("%s/getAssignableRoles" % API().base_url(), "get", {})
+    res = api.https_request("%s/getAssignableRoles" % api.base_url(), "get", {})
 
     for role in json.loads(res.content).get("Role"):
         results.append(boompy.Role(**role))
@@ -21,7 +20,7 @@ def getAssignableRoles():
 
 def executeProcess(process_id, atom_id):
     data = {"processId": process_id, "atomId": atom_id}
-    API().https_request("%s/executeProcess" % API().base_url(), "post", data)
+    api.https_request("%s/executeProcess" % api.base_url(), "post", data)
 
 def provisionPartnerCustomerAccount(data=None):
     """
@@ -33,21 +32,25 @@ def provisionPartnerCustomerAccount(data=None):
     if data is None:
         data = {}
 
+    PROVISION_FIELDS = {"name", "street", "city", "stateCode", "zipCode",
+                        "countryCode", "status", "product"}
+
     # It takes about a minute for this process to complete
-    base_url = "%s/AccountProvision" % API().base_url(partner=True)
+    base_url = "%s/AccountProvision" % api.base_url(partner=True)
 
-    missing_fields = set(PROVISION_FIELDS) - set(data.keys())
+    missing_fields = PROVISION_FIELDS - set(data.keys())
 
-    if len(missing_fields) == 0:
-        res = API().https_request("%s/execute" % base_url, "post", data)
+    if not missing_fields:
+        res = api.https_request("%s/execute" % base_url, "post", data)
         results = json.loads(res.content)
 
+        # According to Boomi docs looks like status will only be pending or completed
         while results.get("status") == "PENDING":
             time.sleep(2)
-            result = API().https_request("%s/%s" % (base_url, results.get("id")), "get", {})
+            result = api.https_request("%s/%s" % (base_url, results.get("id")), "get", {})
             results = json.loads(result.content)
         if results.get("status") != "COMPLETED":
-            raise BoomiError("failed setting up account")
+            raise BoomiError("failed provisioning account, status=%s" % results.get("status"))
         else:
             return results
     else:
